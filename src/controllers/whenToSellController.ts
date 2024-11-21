916a38edfd4a7126710a458374a59ca705cab76d
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2024-11-21 14:22:55 -0300

    feat: sendEmail with games

--- src/controllers/whenToSellController.ts
+++ src/controllers/whenToSellController.ts
@@ -2,56 +2,39 @@ import { Request, Response } from 'express';
 import { getProductIdBySlug, getProductsToListFromSistemaEstoque } from '../services/productService';
 import { bestPriceResearcher, compareById } from '../services/comparisonService';
 import { priceWithFee } from '../helpers/priceWithFee';
+import { IGameToList } from '../interfaces/IGameToList';
+import { sendEmail } from '../services/emailService';
+import { isDateOlderThanMonths } from '../helpers/isDateOlderThanEightMonths';
 
 export const whenToSell = async (req: Request, res: Response): Promise<void> => {
-    const gamesToSell = [];
+    const gamesToSell: IGameToList[] = [];
     try {
-
-        // const bestPrice =await compareById(130959);
-
-        // console.log(bestPrice);
-
-        // const bestPriceWithFee = priceWithFee(bestPrice.menorPreco);
-
-        // console.log(bestPriceWithFee);
 
         // Fazer req
 
         const gamesFromAPI = await getProductsToListFromSistemaEstoque();
-
+        
         // Loop nos jogos
-
         for (const game of gamesFromAPI) {
             // Ver se o menor preco é maior que o minimoParaVenda
-
             const bestPrice = await compareById(Number(game.idGamivo));
 
-            console.log(bestPrice);
-
+            // Pegar o valor do jogo com as taxas
             const bestPriceWithFee = priceWithFee(bestPrice.menorPreco);
 
-            if(bestPriceWithFee > Number(game.minimoParaVenda)){
+            if (bestPriceWithFee > Number(game.minimoParaVenda) || isDateOlderThanMonths(game.dataAdquirida)) { // Jogo que 
+                // Juntar jogos que podem ser listados
                 gamesToSell.push(game);
             }
+        }
+        // console.log(gamesToSell)
 
-            console.log(bestPriceWithFee);
-
-            // Juntar jogos que podem ser listados
-
-
+        // Enviar email avisando os jogos que podem ser listar
+        if (gamesToSell.length > 0) {
+            await sendEmail(gamesToSell);
         }
 
-        // Enviar email avisando que pode listar
-
-
-
-
-        // const productId = await getProductIdBySlug(slug);
-        // console.log(productId);
-
-        // const bestPrice = await bestPriceResearcher(productId);
-
-        res.status(200).json({ message: 'Games successfully checked.' });
+        res.status(200).json({ message: 'Games successfully checked.', gamesToSell });
 
     } catch (error) {
         console.error(error);
